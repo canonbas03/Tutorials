b3e892ff631d0a1f269690af58b6bc7597d1e6ab
canonbas03 <salihcanonbas@gmail.com>
2025-08-04 22:27:01 +0300

    Initial

---
+++ MVCTutorial/MVCTutorial/Controllers/EmployeesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCTutorial.Data;
+using MVCTutorial.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVCTutorial.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly EmployeeContext _context;
+
+        public EmployeesController(EmployeeContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Employees
+        public async Task<IActionResult> Index()
+        {
+            var employees = await _context.Employees.ToListAsync();
+            return View(employees);
+        }
+    }
+}
+
---
+++ MVCTutorial/MVCTutorial/Data/EmployeeContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCTutorial.Models;
+
+namespace MVCTutorial.Data
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
