84d5b7041f5d084d68ec263f389aa3fe354e5d27
canonbas03 <salihcanonbas@gmail.com>
2025-08-05 14:29:18 +0300

    Added Profile photo
    Employee creation action has a file upload field, if no file is added a default picture is assigned

--- MVCTutorial/MVCTutorial/Controllers/EmployeesController.cs
+++ MVCTutorial/MVCTutorial/Controllers/EmployeesController.cs
@@ -27,19 +27,45 @@ namespace MVCTutorial.Controllers
             return View();
         }
 
-        // POST: Employees/Create
+
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Employee employee)
+        public async Task<IActionResult> Create(Employee employee, IFormFile photo)
         {
+            ModelState.Remove("PhotoPath");
+            var errors = ModelState.Values.SelectMany(v => v.Errors);
+            foreach (var error in errors)
+            {
+                Console.WriteLine(error.ErrorMessage);
+            }
+
             if (ModelState.IsValid)
             {
+                if (photo != null && photo.Length > 0)
+                {
+                    var fileName = Path.GetFileName(photo.FileName);
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/employees/", fileName);
+
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await photo.CopyToAsync(stream);
+                    }
+
+                    employee.PhotoPath = "/images/employees/" + fileName;
+                }
+                else
+                {
+                    employee.PhotoPath = "/images/employees/userPhoto.png"; // explicitly set null or just leave it
+                }
+
                 _context.Add(employee);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index)); // Go back to list
+                return RedirectToAction(nameof(Index));
             }
             return View(employee);
         }
+
     }
 }
 
--- MVCTutorial/MVCTutorial/Models/Employee.cs
+++ MVCTutorial/MVCTutorial/Models/Employee.cs
@@ -5,5 +5,8 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal Salary { get; set; }
+
+        // Path to employee image
+        public string? PhotoPath { get; set; }
     }
 }
