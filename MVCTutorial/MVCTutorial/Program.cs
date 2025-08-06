554a432b4f50e993c46b33e64d66951f64ef31d4
canonbas03 <salihcanonbas@gmail.com>
2025-08-06 19:07:13 +0300

    script to accept comma values

--- MVCTutorial/MVCTutorial/Program.cs
+++ MVCTutorial/MVCTutorial/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using MVCTutorial.Data;
 using System.Globalization;
-using Microsoft.AspNetCore.Localization;
 
 namespace MVCTutorial
 {
@@ -9,45 +8,34 @@ namespace MVCTutorial
     {
         public static void Main(string[] args)
         {
-            var cultureInfo = new CultureInfo("tr-TR");
-            cultureInfo.NumberFormat.NumberDecimalSeparator = ",";
-            cultureInfo.NumberFormat.NumberGroupSeparator = ""; // disable thousands separator
-
-            CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
-            CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
-
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
             builder.Services.AddDbContext<EmployeeContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("EmployeeDB")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("EmployeeDB")));
+
 
             var app = builder.Build();
 
-            // Apply localization for model binding
-            var supportedCultures = new[] { cultureInfo };
-            app.UseRequestLocalization(new RequestLocalizationOptions
-            {
-                DefaultRequestCulture = new RequestCulture(cultureInfo),
-                SupportedCultures = supportedCultures,
-                SupportedUICultures = supportedCultures
-            });
-
+            // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+
             app.UseRouting();
+
             app.UseAuthorization();
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Employees}/{action=Index}/{id?}");
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
